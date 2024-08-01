d8448934cbe4428a17ae13205b4a504e0e3b804a
Thomas Wearmouth <tomwmth@pm.me>
2024-08-01 17:54:11 +0800

    Add webhook

--- src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
+++ src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
@@ -3,11 +3,7 @@ package com.zvyap.dailycheckin.test;
 import com.zvyap.dailycheckin.CheckInAction;
 import com.zvyap.dailycheckin.HoyoDaily;
 import com.zvyap.dailycheckin.WebhookInfo;
-import com.zvyap.hoyoapi.APIEnvironment;
-import com.zvyap.hoyoapi.APILocale;
-import com.zvyap.hoyoapi.GameType;
-import com.zvyap.hoyoapi.HoyoToken;
-import com.zvyap.hoyoapi.HoyoverseAPI;
+import com.zvyap.hoyoapi.*;
 import com.zvyap.hoyoapi.feature.daily.DailyCheckInFeature;
 import org.junit.jupiter.api.Test;
 
@@ -23,11 +19,16 @@ public class CheckInDailyTest {
         HoyoDaily.checkIn(feature, List.of(
 
                 CheckInAction.builder()
-                        .token(HoyoToken.of(System.getenv("USER_1_ID"), System.getenv("USER_1_TOKEN")))
+                        .token(HoyoTokenV2.of(System.getenv("USER_1_ID"), System.getenv("USER_1_TOKEN")))
+                        .webhook(WebhookInfo.builder()
+                                .url(System.getenv("WEBHOOK_URL"))
+                                .avatar("https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")
+                                .name("Hoyo Check In")
+                                .build())
                         .game(GameType.GENSHIN_IMPACT)
                         .game(GameType.HONKAI_STAR_RAIL)
+                        .game(GameType.ZENLESS_ZONE_ZERO)
                         .build()
-
 
         ));
     }
